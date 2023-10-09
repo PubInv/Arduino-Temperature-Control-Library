17069dd34b83528ee21e7af32b14a4655f1293b0
Forrest Erickson <forresterickson@comcast.net>
2023-10-09 07:09:30 -0400

    Version for testing with Serial Terminal and Plotter.

--- examples/Multiple/Multiple.ino
+++ examples/Multiple/Multiple.ino
@@ -1,9 +1,19 @@
 // Include the libraries we need
+// Modified by Lee Erickson
+// Date: 230231006
+// Moving Rob's modified library into Arduino IDE library folder Date: 230231008, V3
+
 #include <OneWire.h>
-#include <DallasTemperature.h>
+//#include <DallasTemperature.h>
+//#include "C:\Users\Public\Downloads\Arduino\PubInv\DallasTemprature\Arduino-Temperature-Control-Library\DallasTemperature.h"
+#include "DallasTemperature.h"
+//#include "C:\Users\Public\Downloads\Arduino\PubInv\DallasTemprature\Arduino-Temperature-Control-Library\DallasTemperature.h"
+//#include "..\\DallasTemperature.h"
+
 
 // Data wire is plugged into port 2 on the Arduino
-#define ONE_WIRE_BUS 2
+//#define ONE_WIRE_BUS 2
+#define ONE_WIRE_BUS 5 // For Due in Control v1
 #define TEMPERATURE_PRECISION 9
 
 // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
@@ -25,11 +35,14 @@ DeviceAddress insideThermometer, outsideThermometer;
 void setup(void)
 {
   // start serial port
-  Serial.begin(9600);
-  Serial.println("Dallas Temperature IC Control Library Demo");
+  Serial.begin(115200);
+  Serial.println("V4 Multiple, DALLASTEMPLIBVERSION: 3.99999_2023100");
 
   // Start up the library
   sensors.begin();
+  delay(100);
+  Serial.print("DALLASTEMPLIBVERSION: ");
+  Serial.println(DALLASTEMPLIBVERSION);
 
   // locate devices on the bus
   Serial.print("Locating devices...");
@@ -85,7 +98,8 @@ void setup(void)
   Serial.print("Device 1 Resolution: ");
   Serial.print(sensors.getResolution(outsideThermometer), DEC);
   Serial.println();
-}
+  Serial.println("End of setup");
+}//end setup()
 
 // function to print a device address
 void printAddress(DeviceAddress deviceAddress)
@@ -138,11 +152,21 @@ void loop(void)
 {
   // call sensors.requestTemperatures() to issue a global temperature
   // request to all devices on the bus
-  Serial.print("Requesting temperatures...");
+  //  Serial.print("Requesting temperatures...");
   sensors.requestTemperatures();
-  Serial.println("DONE");
+  //  Serial.println("DONE");
 
   // print the device information
-  printData(insideThermometer);
-  printData(outsideThermometer);
+  //printData(insideThermometer);
+  //printData(outsideThermometer);
+  //  delay(500);
+
+
+  //Print for formating
+  printTemperature(insideThermometer);
+  Serial.print(", ");
+  printTemperature(outsideThermometer);
+  Serial.println();
+
+
 }
